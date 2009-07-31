7d6d53264d0691275e638204dd195bd44c64079b
elizeu.nrj <elizeu.nrj@75e41dd8-7d75-11de-af54-3b18e7082050>
2009-07-31 02:11:52 +0000

    First Commit

---
+++ branches/Alien3DShapes/src/org/igs/android/ogl/engine/shape/Line.java
@@ -0,0 +1,18 @@
+package org.igs.android.ogl.engine.shape;
+
+public class Line {
+	
+/*
+const GLfloat line[] = {
+-0.5f, -0.5f, //point A
+0.5f, -0.5f, //point B
+};
+
+glColor4f(0.0f,1.0f,0.0f,1.0f); //line color
+glVertexPointer(2, GL_FLOAT, 0, line);
+glEnableClientState(GL_VERTEX_ARRAY);
+
+glDrawArrays(GL_LINES, 0, 2);
+*/
+	
+}
